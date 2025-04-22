21210abe32a5f8fb7fadbb70a6314a5f54517860
crazy <axelvasquez582@gmail.com>
2025-04-21 21:38:01 -0500

    agregando ejercicio 2

---
+++ ejercicio2/Perro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio2
+{
+    public class Perro
+    {
+
+        public string name;
+        public string race;
+        public int age;
+
+        public void displayInformation()
+        {
+            Console.WriteLine($"Su nombre es {name} ,su raza es {race} y tiene {age} el perro ");
+        }
+
+
+
+    }
+}
---
+++ ejercicio2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio2
+{
+    internal class Program
+    {
+        //Crear una clase que represente un perro, con propiedades básicas y un método que imprima una descripción
+        //del perro.
+        static void Main(string[] args)
+        {
+            Perro dog = new Perro();
+            dog.name = "miguel";
+            dog.race = "labrador";
+            dog.age= 19;
+            dog.displayInformation();
+
+            Perro dog2 = new Perro();
+            dog2.name = "nike";
+            dog2.race = "negro";
+            dog2.age = 15;
+            dog2.displayInformation();
+
+
+
+        }
+    }
+}
