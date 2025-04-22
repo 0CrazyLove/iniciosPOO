782bbe5eb73811bb7158dc8b02fe2627f880774e
crazy <axelvasquez582@gmail.com>
2025-04-21 20:40:33 -0500

    agregando nuevo proyecto

---
+++ POO/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    public class Person
+    {
+        public string name;
+        public int age;
+
+        public void greet() //greet es saludar
+        {
+
+            Console.WriteLine($"hola mi nombre es {name} y tengo {age} años");
+        }           
+
+
+    }
+}
---
+++ POO/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          Person person1 = new Person();
+            person1.name = "miguel";
+            person1.age = 19;
+            person1.greet();
+
+
+        }
+    }
+}
