72e34fc477447b3425d70c2a44cd5a644832146c
crazy <axelvasquez582@gmail.com>
2025-04-26 23:16:44 -0500

    subiendo archivos de ejercicio6

---
+++ ejercicio6/Person.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio6
+{
+
+    public class Person
+    {
+        //atributos privados
+        private string name;
+        private int age;
+        //mi constructor
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        //metodo para obtener la edad(Getter)
+        public string GetName()
+        {
+            return name;
+        }
+        //metodo set para cambiar la edad (Setter)
+        public void SetName(string Newname)
+        {
+            name = Newname;
+        }
+        //metodo para obtener la edad(Getter)
+        public int GetAge()
+        {
+            return age;
+        }
+        //metodo set para cambiar la edad (Setter)
+        public void SetAge(int NewAge)
+        {
+            if (NewAge >= 0) //validar que la edad no sea negativa
+            {
+                age = NewAge;
+            }
+            else
+            {
+                Console.WriteLine("la edad no puede ser negativa");
+            }
+            
+        }
+
+
+    }
+}
---
+++ ejercicio6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person persona1 = new Person("Miguel", 19);
+            Console.WriteLine($"Nombre: {persona1.GetName()}");
+            Console.WriteLine($"Edad: {persona1.GetAge()}");
+            persona1.SetName("Axel");
+            persona1.SetAge(20);
+            Console.WriteLine($"nuevo nombre: {persona1.GetName()}");
+            Console.WriteLine($"nueva edad: {persona1.GetAge()}");
+            persona1.SetAge(-5); // prueba de la validacion de edad negativa
+
+        }
+    }
+}
