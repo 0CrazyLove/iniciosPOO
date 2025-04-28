04192f8637f9fe0f9248d7f544bceb10a53b253f
crazy <axelvasquez582@gmail.com>
2025-04-28 01:12:02 -0500

    añadiendo archivos de ejercicio8

---
+++ ejercicio8/Employee.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio8
+{
+    internal class Employee
+    {
+        //atributos privados
+
+        private string name;
+        private double salary;
+        private string employeeStatus;
+
+        //propiedad para el nombre 
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    Console.WriteLine("EL nombre no es valido");
+
+                }
+                else
+                {
+                    name = value;
+                }
+
+            }
+
+        }
+
+        //propiedad para el salario
+
+        public double Salary
+        {
+            get { return salary; }
+            set
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("El salario debe ser mayor a 0");
+
+                }
+                else
+                {
+                    salary = value;
+                }
+            }
+
+        }
+        //propiedad para el estado del empleado
+        public string EmployeeStatus
+        {
+           get { return employeeStatus; }
+            set
+            {
+                if (value == "activo" || value == "inactivo")
+                {
+                    employeeStatus = value;
+                }
+                else
+                {
+                    Console.WriteLine("El estado del empleado solo debe ser activo o inactivo");
+                }
+                
+
+
+            }
+        }
+
+        //constructor
+        public Employee(string name, double salary, string employeeStatus)
+        {
+            Name = name;
+            Salary = salary;
+            EmployeeStatus = employeeStatus;
+        }
+
+
+        //metodo para mostrar los datos del empleado 
+        public void DisplayInformation()
+        {
+            Console.WriteLine($"Nombre: {Name}");
+            Console.WriteLine($"Salario: {Salary.ToString("f3")}COP");
+            Console.WriteLine($"Estado de empleo: {EmployeeStatus}");
+
+
+
+        }
+
+
+    }
+}
---
+++ ejercicio8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employe1 = new Employee("Miguel", 5.000, "inactivo");
+            employe1.DisplayInformation();
+            Console.WriteLine("------------------------valores cambiados-------------------------");
+
+
+            employe1.Name = "Axel";
+            employe1.Salary = 20.000;
+            employe1.EmployeeStatus = "activo";
+            employe1.DisplayInformation();
+
+            Console.WriteLine(employe1.Name);
+
+
+
+
+
+        }
+    }
+}
