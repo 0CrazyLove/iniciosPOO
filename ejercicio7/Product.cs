e3c4db635bf00c54b6a9bd118b3041c4807471c9
crazy <axelvasquez582@gmail.com>
2025-04-27 23:33:53 -0500

    subiendo archivos de ejercicio7

---
+++ ejercicio7/Product.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio7
+{
+    public class Product
+    {
+        //atributos privados
+        private string name;
+        private double price;
+        //constructor
+        public Product(string name, double price)
+        {
+            if(price <= 0)
+            {
+                Console.WriteLine("El precio no es valido");
+                Environment.Exit(0);
+            }
+            
+            if(string.IsNullOrEmpty(name))
+            {
+                Console.WriteLine("El nombre no es valido");
+                Environment.Exit(0);
+            }
+
+            this.name = name;
+            this.price = price;
+
+        }
+
+        //metodo para obtener el nombre(Getter)
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        //metodo para obtener el precio(Getter)
+        public double GetPrice()
+        {
+            
+            return price;
+        }
+
+        //metodo set para cambiar el nombre(Setter)
+        public void SetName(string newName)
+        {
+            if (string.IsNullOrEmpty(newName))
+            {
+                Console.WriteLine("El nombre no es valido");
+            }
+            else
+            {
+                name = newName;
+
+            }
+
+        }
+        //metodo set para cambiar el precio(Setter)
+        public void SetPrice(double newPrice)
+        {
+            if (newPrice >= 0)
+            {
+                price = newPrice;
+            }
+            else
+            {
+                Console.WriteLine("El precio no es valido");
+            }
+            
+
+
+        }
+
+
+
+    }
+}
---
+++ ejercicio7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product producto = new Product("tomate", 5.000);
+            Console.WriteLine($"Producto: {producto.GetName()}");
+            Console.WriteLine($"Precio: {producto.GetPrice().ToString("f2")}COP");
+            //modificando nombre
+
+            Console.WriteLine("-----------------------------------------------------Productos cambiados-----------------------------------------------");
+            producto.SetName("limon");
+            producto.SetPrice(200);
+            Console.WriteLine($"Producto: {producto.GetName()}");
+            Console.WriteLine($"precio: {producto.GetPrice()}COP");
+
+        }
+    }
+}
