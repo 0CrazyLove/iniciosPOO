8499303c3ec1a0bc8f65e5164a0c13942a9c0d9a
crazy <axelvasquez582@gmail.com>
2025-04-29 23:33:52 -0500

    subiendo ejercicio9

---
+++ ejercicio9/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Introduce tu nombre: ");
+            string nameUsario = Console.ReadLine();
+            Console.Write("Introduce tu contraseña: ");
+            int passwordUsuario = 0;
+            try
+            {
+                passwordUsuario = int.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                
+                Console.WriteLine("Error: solo se permite numeros");
+                Main(args);
+
+            }
+                Usuarios usuario = new Usuarios(nameUsario, passwordUsuario);
+            Console.WriteLine("--------------------------------------------------------------------------------- ");
+            usuario.DisplayInformation();
+            Console.WriteLine("¿Quieres cambiar tu contraseña? (si/no)");
+            string answer = Console.ReadLine();
+            if (answer.ToLower() == "si")
+            {
+                Console.Write("Introduce tu nueva contraseña: ");
+                int passwordNew = 0;
+                try
+                {
+                    passwordNew = int.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+
+                    Console.WriteLine("Error: solo se permite numeros");
+
+                }
+
+                Usuarios usuario2 = new Usuarios(nameUsario, passwordNew);
+                Console.WriteLine("--------------------------------------------------------------------------------- ");
+                usuario2.DisplayInformation();
+
+
+
+            }
+            else
+            {
+                Console.WriteLine("Gracias por usar el programa");
+            }
+
+
+
+
+
+        }
+    }
+}
---
+++ ejercicio9/Usuarios.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio9
+{
+    public class Usuarios
+    {
+        private string name;
+        private int password;
+
+        //constructor
+        public Usuarios(string Name, int Password)
+        {
+            name = Name;
+            password = Password;
+        }
+
+        //metodo de cambiar el name
+
+        public string Name
+        {
+
+            get { return name; }
+            set
+            {
+
+                if (string.IsNullOrEmpty(value))
+                {
+
+
+                    Console.WriteLine("Error: nombre no valido");
+
+                }
+                else
+                {
+                    name = value;
+                }
+
+            }
+        }
+
+        //metodo para cambiar la comtraseña
+        public int Password
+        {
+
+            get { return password; }
+            set
+            {
+                if (value == password)
+                {
+                    password = value;
+
+                }
+                else
+                {
+
+                    Console.WriteLine("Contraseña incorrecta");
+                }
+
+            }
+
+        }
+
+
+        //metodo para mostrar la informacion
+        public void DisplayInformation()
+        {
+            Console.WriteLine($"Nombre: {Name}");
+            Console.WriteLine($"Contraseña: {Password}");
+        }
+
+
+    }
+}
