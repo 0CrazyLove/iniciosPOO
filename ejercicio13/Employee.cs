bf2b0d36cc11c3f2b45a060937220ec309a74eab
crazy <axelvasquez582@gmail.com>
2025-05-01 22:07:43 -0500

    añadiendo ejercicio13

---
+++ ejercicio13/Employee.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio13
+{
+    class Employee
+    {
+        //atributos protegidos
+        protected string name;
+        protected double salary;
+        //constructor
+        public Employee(string name, double salary)
+        {
+            this.name = name;
+
+            if (salary <= 0)
+            {
+                Console.WriteLine("No se permite numeros negativos");
+            }
+            else
+            {
+                this.salary = salary;
+
+            }
+
+
+        }
+        //metodo para mostrar informacion
+        public void DisplayInformationEmployee()
+        {
+            Console.Clear();
+            Console.WriteLine("---------------------------------------------------");
+            Console.WriteLine($"Nombre del empleado: {name}");
+            Console.WriteLine($"Salario del empleado: {salary.ToString("f3")}");
+            
+
+
+
+        }
+
+    }
+    //clase derivada de la clase base Employee
+    class Manager : Employee
+    {
+        //atributo adiicional
+        protected string department;
+        public Manager(string name, double salary, string department) : base(name, salary)
+        {
+
+            this.department = department;
+
+        }
+        //metodo para mostrar que departamento gestiona
+        public void DisplayDepartment()
+        {
+            Console.WriteLine($"Departamento que gestiona {name}: {department}");
+            Console.WriteLine("---------------------------------------------------");
+        }
+
+    }
+
+    class Developer : Employee
+    {
+        //atributo adicional
+        protected string lenguageFavorite;
+        //constructor
+        public Developer(string name , double salary , string lenguageFavorite) : base(name , salary)
+        {
+            this.lenguageFavorite = lenguageFavorite;
+
+
+        }
+        //metodo para obtener la informacion de que lenguaje utiliza
+        public void DisplayLenguage()
+        {
+            Console.WriteLine($"lengueje favorito de{name}: {lenguageFavorite}");
+            Console.WriteLine("---------------------------------------------------");
+        }
+        
+
+        
+
+    }
+
+}
---
+++ ejercicio13/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("gerente o desarrolador?");
+            string employee = Console.ReadLine();
+            if (employee.ToLower() == "gerente")
+            {
+                //isntancia del objeto Manager
+                Console.Write("Nombre del gerente: ");
+                string nameManager = Console.ReadLine();
+                string inputSalary;
+                double salaryManager = 0;
+                do
+                {
+                    Console.Write("Salario: ");
+                    inputSalary = Console.ReadLine();
+                } while (!double.TryParse(inputSalary, out salaryManager));
+                Console.Write("Departamento que gestiona: ");
+                string department = Console.ReadLine();
+                Manager manager = new Manager(nameManager, salaryManager, department);
+                manager.DisplayInformationEmployee();
+                manager.DisplayDepartment();
+            }
+            else if (employee.ToLower() == "desarrolador")
+            {
+
+
+                //istancia del objeto developer
+                Console.Write("Nombre del desarrolador: ");
+                string nameDeveloper = Console.ReadLine();
+                string inputSalaryDeveloper;
+                double salaryDeveloper = 0;
+                do
+                {
+                    Console.Write("Salario: ");
+                    inputSalaryDeveloper = Console.ReadLine();
+                } while (!double.TryParse(inputSalaryDeveloper, out salaryDeveloper));
+
+                Console.Write("lenguaje especializado: ");
+                string lenguageDeveloper = Console.ReadLine();
+                Developer developer = new Developer(nameDeveloper, salaryDeveloper, lenguageDeveloper);
+                developer.DisplayInformationEmployee();
+                developer.DisplayLenguage();
+            }
+            else
+            {
+                Console.WriteLine("Error. trabajo incorrecto \n Intente de nuevo: ");
+                Main(args);
+
+            }
+
+
+
+
+        }
+    }
+}
