feb09ef3e5a3301f0f2499d3e4a0ea08769f1281
crazy <axelvasquez582@gmail.com>
2025-05-01 19:42:22 -0500

    agregando ejercicio11

---
+++ ejercicio11/Animal.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio11
+{
+    class Animal
+    {
+        //atributo protegido
+        protected string name;
+
+        //constructor
+        public Animal(string name)
+        {
+            this.name = name;
+        }
+        public void Breathe()
+        {
+            Console.WriteLine($"{name} esta respirando");
+        }
+    }
+
+    //clase derivada
+    class Perro : Animal
+    {
+        public Perro(string name) : base(name)
+        {
+        }
+        //metodo exclusivo de la clase hija
+        public void Bark()
+        {
+            Console.WriteLine($"{name} esta ladrando");
+
+
+
+        }
+    }
+
+
+
+
+
+
+}
---
+++ ejercicio11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Perro perro1 = new Perro("perro");
+            perro1.Breathe();
+            perro1.Bark();
+
+
+
+        }
+    }
+}
