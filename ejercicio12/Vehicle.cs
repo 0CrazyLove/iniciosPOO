a35fa09c5244ced0039d37f742f600adf6037ef5
crazy <axelvasquez582@gmail.com>
2025-05-01 20:18:53 -0500

    agregando ejercicio12

---
+++ ejercicio12/Vehicle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio12
+{
+    class Vehicle
+    {
+        protected string brand; //"brand" significa vehiculo en ingles
+
+
+        public Vehicle(string brand)
+        {
+            this.brand = brand;
+        }
+
+        public void DisplayInformation()
+        {
+            Console.WriteLine($"este vehiculo es de la marca {brand}");
+        }
+
+    }
+
+    class Car : Vehicle
+    {
+        public Car(string brand) : base(brand)
+        {
+
+        }
+
+        public void Iformation() {
+            Console.WriteLine($"este vehiculo de la marca: {brand} enciende");
+        
+
+
+        }
+    
+    
+    }
+
+
+}
