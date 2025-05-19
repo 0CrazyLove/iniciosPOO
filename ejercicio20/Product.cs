f1b96ad3687ea0f279281593ef9c7c355d1f1646
0CrazyLove <axelvasquez582@gmail.com>
2025-05-18 19:44:08 -0500

    haciendo el ejercicio

---
+++ ejercicio20/Product.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio20
+{
+    class Product
+    {
+        private string name;
+        private decimal price;
+
+        public Product(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (!String.IsNullOrEmpty(value))
+                {
+                    this.name = value;
+                }
+                else
+                {
+                    Console.WriteLine("Error. nombre no valido");
+                }
+            }
+        }
+        public decimal Price
+        {
+            get { return price; }
+            set
+            {
+                if (!(value <= 0))
+                {
+                    this.price = value;
+                }
+                else
+                {
+                    Console.WriteLine("Error. no se permite valores negativos");
+                }
+            }
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"nombre del producto: {Name} \n precio del producto: ${Price}COP ");
+        }
+        
+
+    }
+}
--- ejercicio20/Program.cs
+++ ejercicio20/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+            products.Add(new Product("Shampo", 200.000m));
+            products.Add(new Product("pc", 200.000m));
+            products.Add(new Product("cargador", 39.000m));
+            products.Add(new Product("dildo", 23.000m));
+            products.Add(new Product("mouse", 123.000m));
+
+            foreach (Product product in products)
+            {
+                if (product.Price >= 50.000m)
+                {
+                    product.DisplayInfo();
+                }
+
+            }
+
+        }
+
+
+    }
+}
