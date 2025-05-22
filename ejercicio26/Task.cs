f1b438e7625f0f4a4bd58f56d922c9fc38d0dc3e
0CrazyLove <axelvasquez582@gmail.com>
2025-05-22 10:52:08 -0500

    corrigiendo errores

---
+++ ejercicio26/Task.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio26
+{
+
+    public class Task
+    {
+        
+        //atributos propios de Task
+        private string name;
+        private int priority;
+
+        //propiedad de name
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (!String.IsNullOrEmpty(value))
+                {
+                    this.name = value;
+                }
+                else
+                {
+                    System.Console.WriteLine("Error. Nombre no valido");
+                }
+            }
+        }
+        public int Priority
+        {
+            get { return priority; }
+            set
+            {
+                if (!(value <=0) )
+                {
+                    this.priority = value;
+                }
+                else
+                {
+                    System.Console.WriteLine("Error. no se puede asignar valores nulos o negativos");
+                }
+            }
+        }
+        
+    }
+
+}
