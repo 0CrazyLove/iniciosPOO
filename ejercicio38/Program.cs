87b22f35595341b81d9f89ca942dfabc61cfc090
0CrazyLove <axelvasquez582@gmail.com>
2025-05-27 10:06:28 -0500

    resolviendo erroes

--- ejercicio38/Program.cs
+++ ejercicio38/Program.cs
@@ -27,8 +27,6 @@ namespace ejercicio38
                 bool exito = int.TryParse(Console.ReadLine(), out option);
                 if (exito)
                 {
-
-
                     if (option.Equals(1))
                     {
                         System.Console.WriteLine(@" 
@@ -45,25 +43,48 @@ namespace ejercicio38
                             {
                                 System.Console.WriteLine("Has elegido: Servicio de Diseño.");
                                 System.Console.Write("Ingrese el nombre del servicio: \n>");
-                                string? nameService = Console.ReadLine();
+                                string? nameServiceDesigner = Console.ReadLine();
                                 System.Console.Write("Ingrese la descripción: \n>");
-                                string? descriptionService = Console.ReadLine();
+                                string? descriptionServiceDesigner = Console.ReadLine();
                                 System.Console.Write("Ingrese el precio basae: \n>");
-                                decimal priceBaseService;
-                                bool exitoPriceBaseService = decimal.TryParse(Console.ReadLine(), out priceBaseService);
+                                decimal priceBaseServiceDesigner;
+                                bool exitoPriceBaseService = decimal.TryParse(Console.ReadLine(), out priceBaseServiceDesigner);
                                 if (!exitoPriceBaseService)
                                 {
                                     throw new Exception("Escriba un precio valido.");
                                 }
-                                ServiceDesigner serviceDesigner = new ServiceDesigner(nameService!, descriptionService!, priceBaseService);
+                                ServiceDesigner serviceDesigner = new ServiceDesigner(nameServiceDesigner!, descriptionServiceDesigner!, priceBaseServiceDesigner);
 
 
+                            }
+                            else if (option.Equals(2))
+                            {
+                                System.Console.WriteLine("Has elegido: Servicio de Programacion.");
+                                System.Console.Write("Ingrese el nombre del servicio: \n>");
+                                string? nameServiceDeveloper = Console.ReadLine();
+                                System.Console.Write("Ingrese la descripción: \n>");
+                                string? descriptionServiceDeveloper = Console.ReadLine();
+                                System.Console.Write("Ingrese el precio basae: \n>");
+                                decimal priceBaseServiceDeveloper;
+                                bool exitoPriceBaseServiceDeveloper = decimal.TryParse(Console.ReadLine(), out priceBaseServiceDeveloper);
+                                if (!exitoPriceBaseServiceDeveloper)
+                                {
+                                    throw new Exception("Escriba un precio valido.");
+                                }
+                                System.Console.Write("Ingrese la cantidad de hora de servicio \n>");
+                                int hours;
+                                bool exitoHours = int.TryParse(Console.ReadLine(), out hours);
+                                if(!exitoHours){
+                                    throw new Exception("Ingrese una cantidad de hora valida.");
+                                }
+                                ServiceDeveloper serviceDeveloper = new ServiceDeveloper(nameServiceDeveloper!, descriptionServiceDeveloper! , priceBaseServiceDeveloper, hours);
                             }
 
                         }
 
 
                     }
+                   
 
 
                 }
